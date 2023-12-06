1cd2f0d5b0c14452cdce53a7f74df728d7866d13
Gino Grippo <gino.carlo02@gmail.com>
2023-12-06 10:35:34 -0300

    More button animations

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,6 +7,8 @@ public class MainMenu : MonoBehaviour
 {
 
     private GameObject optionsUI;
+    private Animator optionsUIAnim;
+    private bool optionsWasPressed = false;
 
     private void Start()
     {
@@ -20,7 +22,16 @@ public class MainMenu : MonoBehaviour
 
     public void HandleOptionsButtonOnClickEvent()
     {
-        optionsUI.SetActive(!optionsUI.activeSelf);
+        if (optionsWasPressed)
+        {
+            optionsWasPressed = false;
+            optionsUIAnim.Play("HideOptionsUI");
+        }
+        else
+        {
+            optionsWasPressed = true;
+            optionsUIAnim.Play("ShowOptionsUI");
+        }
     }
 
     public void HandleQuitButtonOnClickEvent()
@@ -30,7 +41,8 @@ public class MainMenu : MonoBehaviour
 
     private void InitUI()
     {
-        optionsUI = GameObject.FindGameObjectWithTag("OptionsUI");
-        optionsUI.SetActive(false);
+        //optionsUI = GameObject.FindGameObjectWithTag("OptionsUI");
+        optionsUIAnim = GameObject.FindGameObjectWithTag("OptionsUI").GetComponent<Animator>();
+        //optionsUI.SetActive(false);
     }
 }
